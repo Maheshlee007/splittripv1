315c31a175f266de4952433bf8d036075dce84c7
MaheshLee <maheshdharapureddy@gmail.com>
2026-05-22 06:57:49 +0000

    upte configS

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig(({ mode }) => ({
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
-        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024, // 4MB
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
